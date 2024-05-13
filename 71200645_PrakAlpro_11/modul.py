8135da3a1423881e2e7f05edc0c25f2edc7e81d1
EdithFelicia <78738551+EdithFelicia@users.noreply.github.com>
2024-05-14 01:01:23 +0700

    Add files via upload

--- 71200645_PrakAlpro_11/modul.py
+++ 71200645_PrakAlpro_11/modul.py
@@ -1,11 +1,22 @@
-# t1 = ("a")
-# t2 = "a"
-# print(t1)
-# t3 = tuple()
-# print(t3)
+# t1 = ("Kristus")
+# print(type(t1))
+# t2 = ("Yesus",)
+# print(type(t2))
 
 # elemen pada tuple tidak dapat berubah.
 # tapi bisa diganti dengan elemen lain berdasarkan index
+
+# tuple_1 = ()
+# tuple_2 = tuple()
+
+
+# kata = ("Akhir", "Zaman", "Tiba")
+# print(kata[1])
+
+# tuple_1 = "Yesus", "mengasihi", "kamu"
+# tuple_2 = "selama", "lamanya"
+# tuple_3 = (tuple_1, tuple_2)
+# print(tuple_3)
 
 # TIDAK BEKERJA
 # t3 = tuple("dutawacana")
@@ -13,9 +24,21 @@
 # print(type(t3))
 # print(t3)
 
-# t3 = tuple("dutawacana")
-# t3 = ("D",) + t3[1:]
-# print(t3)
+# kata = tuple(["Beruang", "Kucing", "Panda", "Kupu-kupu"])
+# kata = ("Penguin",) + kata[1:]
+# print(kata)
+# print(kata[0:1])
+# print(kata[0:2])
+# print(kata[1:3])
+# print(kata[0:-1])
+# print(kata[-1:-3])
+# print(kata[-1:3])
+# print(kata[-3:-1])
+
+# a = (1, 2, 3)
+# b = (50, 60, 70)
+# c = a + b
+# print(c)
 
 # kalimat = 'but soft what light in yonder window breaks'
 # dafkata = kalimat.split()
@@ -43,11 +66,11 @@
 # t = list(d.items())
 # print(t)
 
-# d = {"a": 10, "b": 1, "c": 22}
-# t = list()
-# for key, val in d.items():
-#     t.append((val, key))
-# print(t)
+# kata = {"a": 10, "b": 1, "c": 22}
+# daftar = list()
+# for key, value in kata.items():
+#     daftar.append((value, key))
+# print(daftar)
 
 # import string
 # fhand = open("romeo-full.txt")
@@ -71,3 +94,46 @@
 # for key, val in lst[:]:
 #     print(key, val)
 
+# def nilai_rata_tertinggi(murid):
+#     avg_tertinggi = 0
+#     murid_tertinggi = None
+
+#     for i in murid:
+#         nama, nilai = i
+#         avg = sum(nilai) / len(nilai)
+
+#         if avg > avg_tertinggi:
+#             avg_tertinggi= avg
+#             murid_tertinggi = nama
+
+#     return murid_tertinggi, avg_tertinggi
+
+# murid = [("Yesaya", [85, 90, 92]), ("Yeremia", [78, 85, 80]), ("Yehezkiel", [90, 92, 95])]
+# murid_tertinggi, avg_tertinggi = nilai_rata_tertinggi(murid)
+
+# print("Nilai rata tertinggi")
+# print("Nama:", murid_tertinggi)
+# print("Nilai rata-rata:", avg_tertinggi)
+
+# kata = ("Sampai jumpa", "saudaraku")
+# pertama, kedua = kata
+# print(pertama)
+# print(kedua)
+
+# kata = "Sampai jumpa", "saudaraku"
+# (pertama, kedua) = kata
+# print(pertama)
+# print(kedua)
+
+# kata = "Sampai jumpa", "saudaraku"
+# pertama = kata[0]
+# kedua = kata[1]
+# print(pertama)
+# print(kedua)
+
+daftar = ("Dosa", "Dunia", 2000, 2024)
+print(daftar)
+del daftar
+
+daftar = ("Hidup", "Suci", 2024)
+print("Daftar baru:", daftar)
