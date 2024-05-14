d1fb459b4c55c503027173ba4a217f25e3bfee39
EdithFelicia <78738551+EdithFelicia@users.noreply.github.com>
2024-05-14 23:39:59 +0700

    Add files via upload

--- 71200645_PrakAlpro_11/Latihan 11.3.py
+++ 71200645_PrakAlpro_11/Latihan 11.3.py
@@ -1,16 +1,22 @@
 def hitung_jam(filename):
     with open(filename, 'r') as file:
-        jam_email = dict()
+        jam_email = []
 
         for i in file:
             if i.startswith("From "):
                 waktu = i.split()[5]
                 jam = waktu.split(':')[0]
-                jam_email[jam] = jam_email.get(jam, 0) + 1
+                jam_email.append((jam, 1))
 
-    for jam, jumlah in sorted(jam_email.items()):
+    hitung_email = {}
+    for jam, _ in jam_email:
+        hitung_email[jam] = hitung_email.get(jam, 0) + 1
+
+    jam_email_tuple = list(hitung_email.items())
+    jam_email_tuple.sort()
+
+    for jam, jumlah in jam_email_tuple:
         print(jam, jumlah)
 
-filename = ("mbox-short.txt")
-
+filename = "mbox-short.txt"
 hitung_jam(filename)
